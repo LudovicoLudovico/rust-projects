9dff19edfe7dae1c32af3c7abf63c12e076df4a4
Ludovico Furlanetto <ludovico.furlanetto@gmail.com>
2023-01-15 22:02:57 +0100

    improved matrix input and parsing

--- lin_alg/src/main.rs
+++ lin_alg/src/main.rs
@@ -5,8 +5,8 @@ mod rational;
 use crate::matrix::Matrix;
 
 fn main() {
-    let mut mat1 = Matrix::new_from_string("{-1/2,2,3},{4,5,6},{7,8,9}".to_string());
-    let mat2 = Matrix::new_from_string("{1/2,-2,-3},{-4,-5,-6},{-7,-8,-9}".to_string());
+    let mut mat1 = Matrix::new_from_string("-1/2,2,3 | 4,5,6 | 7,8,9".to_string());
+    let mat2 = Matrix::new_from_string("1/2,-2,-3 | -4,-5,-6 | -7,-8,-9".to_string());
 
     mat1.print();
 
--- lin_alg/src/matrix.rs
+++ lin_alg/src/matrix.rs
@@ -31,41 +31,29 @@ impl Matrix {
     }
 }
 
+// INPUT 1,3,4 | 5,6,7 | 9,10,11
 fn parse_to_matrix(mut input: String) -> Matrix {
-    let mut row_num = 0;
-    let mut col_num = 0;
-    let mut stop_counting_cols = false;
+    input.retain(|c| !c.is_whitespace());
+    let tokens: Vec<&str> = input.split('|').collect();
 
-    for char in input.chars() {
-        if char == '{' {
-            row_num += 1;
-            continue;
-        } else if char == ',' && !stop_counting_cols {
-            col_num += 1;
-            continue;
-        } else if char == '}' {
-            stop_counting_cols = true;
+    let mut result = vec![];
+
+    let mut i = 0;
+    let mut j = 0;
+
+    while i < tokens.len() {
+        let tks: Vec<&str> = tokens[i].split(',').collect();
+        result.push(vec![]);
+        j = 0;
+
+        while j < tks.len() {
+            result[i].push(Rational::new_from_string(&tks[j].to_string()));
+            j += 1;
         }
+        i += 1;
     }
 
-    col_num += 1;
-
-    input.retain(|c| (c != '{' && c != '}' && !c.is_whitespace()));
-
-    let mut result = Matrix(vec![vec![Rational::new_default(); col_num]; row_num]);
-
-    let tokens: Vec<&str> = input.split(',').collect();
-
-    let mut k = 0;
-    for row in result.0.iter_mut() {
-        for col in row.iter_mut() {
-            *col = Rational::new_from_string(&tokens[k].to_string());
-
-            k += 1;
-        }
-    }
-
-    result
+    Matrix(result)
 }
 
 impl AddAssign<&Matrix> for Matrix {
